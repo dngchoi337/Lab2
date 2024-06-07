48560e331248dbba48a9a27e3986a3e2c14a599e
yoonchae <dngchoi337@snu.ac.kr>
2024-06-07 08:25:11 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
@@ -7,16 +8,22 @@ int N,M;
 vector<int> vec;
 
 void func(int cnt) {
+	if(vec.size() == M){
+		sort(vec.begin(), vec.end());
+		for(int num : vec){
+			cout << num << " ";
+		}
+		cout << endl;
+	}
 
-
-
-
-
-
-
+	for(int i = cnt; i <= N; i++){
+		vec.push_back(i);
+		func(i+1);
+		vec.pop_back();
+	}
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(1);
 }
